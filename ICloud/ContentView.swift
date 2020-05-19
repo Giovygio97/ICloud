c88904d20652f8c453c5e55208ed2e78c94fd640
Giovanni Di Guida <diguida.gio@gmail.com>
2020-05-19 10:47:07 +0200

    init

---
+++ ICloud/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ICloud
+//
+//  Created by Giovanni Di Guida on 19/05/2020.
+//  Copyright © 2020 Giovanni Di Guida. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
